b5157df9904c519289077e4870ee791f7c90a03f
Riley Curtis <rileycurtis@mail.weber.edu>
2016-09-29 13:08:32 -0600

    Updated House_Baratheon.sh

--- House_Baratheon.sh
+++ House_Baratheon.sh
@@ -1,1 +1,35 @@
-This is some text
+#/bin/bash/
+
+#House Baratheon
+#HW3
+
+usage() {
+	echo "USAGE: ./House_Baratheon.sh [-a awkFile] [-s sedFile] [-i inputFile]"
+	echo "All three optiions are required"
+}
+
+if [[ ${#} -ne 3 ]] || [[ ${1} == "--help" ]]
+then
+	usage
+	exit 1
+fi
+
+while getopts ":a:s:i:" opt;
+do
+	case opt in
+		a)
+			#TODO
+			;;
+		s)
+			#TODO
+			;;
+		i)
+			#TODO
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
+exit 0
